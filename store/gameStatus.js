6cd3243eef467b9a3d4207e4c255657311c6b3fe
paperhsiao <destiny5420@gmail.com>
2022-05-20 09:36:50 +0800

    feat: 修正 localStorageIndex

--- store/gameStatus.js
+++ store/gameStatus.js
@@ -6,7 +6,7 @@ const STATUS = {
 
 const UNIT_SCORE = 100
 const VICTORY_MAX_COUNT = 10
-const LOCAL_STORAGE_INDEX = 0
+const LOCAL_STORAGE_INDEX = 10
 let curAnimateEnd = false
 let curGameStatus = STATUS.IDLE
 let isExample = false
